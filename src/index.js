ce6bd94eb57fdae46e8e5434af574662f36f5bc2
Molchanov Vladimir <molchanov.vladimir1993@gmail.com>
2022-03-27 21:55:44 +0300

    Create Store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function createStore(initialState) {
+    let state = initialState
+
+    function getState() {
+        return state
+    }
+
+    return {getState}
+}
+const store = createStore([{id: 1, description: "Task 1", completed: false}])
+
 const App = (params) => {
     return (
         <h1>App</h1>
